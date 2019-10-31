755fd74b334418502c40089f3add6ba557e586db
z01329125 <z01329125@gmail.com>
2019-10-31 23:40:00 +0800

    3D按鈕上傳

--- musicgame/Assets/FancyScrollView/Examples/Sources/02_FocusOn/Cell.cs
+++ musicgame/Assets/FancyScrollView/Examples/Sources/02_FocusOn/Cell.cs
@@ -6,11 +6,15 @@ namespace FancyScrollView.Example02
     public class Cell : FancyScrollViewCell<ItemData, Context>
     {
         [SerializeField] Animator animator = default;
-        [SerializeField] Text message = default;
+        //   [SerializeField] Text message = default;
+        [SerializeField] Text messageLarge = default;
         [SerializeField] Image image = default;
         [SerializeField] Image imageLarge = default;
         [SerializeField] Button button = default;
-
+        [SerializeField] Button button3 = default;
+        [SerializeField] Button button6 = default;
+        public GameObject page;
+        public GameObject page2;
         static class AnimatorHash
         {
             public static readonly int Scroll = Animator.StringToHash("scroll");
@@ -19,17 +23,46 @@ namespace FancyScrollView.Example02
         void Start()
         {
             button.onClick.AddListener(() => Context.OnCellClicked?.Invoke(Index));
+            button3.onClick.AddListener(Active_Text);
+            button6.onClick.AddListener(Active_Text2);
+        }
+        public void Active_Text()
+        {
+            if (!page.activeInHierarchy)
+            { page.SetActive(true); }
+            else
+            { page.SetActive(false); }
         }
 
+        public void Active_Text2()
+        {
+            if (!page2.activeInHierarchy)
+            { page2.SetActive(true); }
+            else
+            { page2.SetActive(false); }
+        }
         public override void UpdateContent(ItemData itemData)
         {
-            message.text = itemData.Message;
+            //  message.text = itemData.Message;
+            messageLarge.name = itemData.Message;
             imageLarge.sprite = Resources.Load<Sprite>(itemData.imageName);
-         /*   var selected = Context.SelectedIndex == Index;
-            image.color = selected
-                ? new Color32(0, 255, 255, 100)
-                : new Color32(255, 255, 255, 77);
-         */
+          var selected = Context.SelectedIndex == Index;
+            /*   image.color = selected
+                 ? new Color32(0, 255, 255, 100)
+                 : new Color32(255, 255, 255, 77);
+          */
+            if (selected)
+            {
+                button3.gameObject.SetActive(true);
+                button6.gameObject.SetActive(true);
+            }
+            else
+            {
+                button3.gameObject.SetActive(false);
+                button6.gameObject.SetActive(false);
+                page.SetActive(false);
+                page2.SetActive(false);
+            }
         }
 
         public override void UpdatePosition(float position)
